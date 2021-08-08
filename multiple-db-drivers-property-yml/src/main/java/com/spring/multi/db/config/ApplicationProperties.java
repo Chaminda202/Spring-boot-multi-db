07e8a84d9126fee95c693bbde0525b35c07bc8c7
Sampath <sampath.dapanadurage@ncs.com.sg>
2021-08-08 18:21:55 +0800

    define multi database configuration in yml file

---
+++ multiple-db-drivers-property-yml/src/main/java/com/spring/multi/db/config/ApplicationProperties.java
@@ -0,0 +1,101 @@
+package com.spring.multi.db.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "spring.datasource")
+@Getter
+@Setter
+public class ApplicationProperties {
+    private final ApplicationProperties.Base base = new ApplicationProperties.Base();
+    private final ApplicationProperties.ThirdParty thirdParty = new ApplicationProperties.ThirdParty();
+    public static class Base {
+        private String driverClassName;
+        private String url;
+        private String username;
+        private String password;
+
+
+        public String getDriverClassName() {
+            return driverClassName;
+        }
+
+        public void setDriverClassName(String driverClassName) {
+            this.driverClassName = driverClassName;
+        }
+
+        public String getUrl() {
+            return url;
+        }
+
+        public void setUrl(String url) {
+            this.url = url;
+        }
+
+        public String getUsername() {
+            return username;
+        }
+
+        public void setUsername(String username) {
+            this.username = username;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+    }
+
+    public static class ThirdParty {
+        private String driverClassName;
+        private String url;
+        private String username;
+        private String password;
+
+        public String getDriverClassName() {
+            return driverClassName;
+        }
+
+        public void setDriverClassName(String driverClassName) {
+            this.driverClassName = driverClassName;
+        }
+
+        public String getUrl() {
+            return url;
+        }
+
+        public void setUrl(String url) {
+            this.url = url;
+        }
+
+        public String getUsername() {
+            return username;
+        }
+
+        public void setUsername(String username) {
+            this.username = username;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+    }
+
+    public Base getBase() {
+        return base;
+    }
+
+    public ThirdParty getThirdParty() {
+        return thirdParty;
+    }
+}
